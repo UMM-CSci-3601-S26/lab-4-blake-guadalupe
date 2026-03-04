8b2f136f124a7f08d82fb315c7e3c5b58c630aed
Thatcher Werner <werne662@morris.umn.edu>
2026-03-04 15:53:21 -0600

    Reverting Deletion Pt3
    Commented out non inventory/supply e2e tests to stop them from running

--- client/cypress/e2e/company-list.cy.ts
+++ client/cypress/e2e/company-list.cy.ts
@@ -1,36 +1,36 @@
-import { CompanyListPage } from '../support/company-list.po';
+// import { CompanyListPage } from '../support/company-list.po';
 
-const page = new CompanyListPage();
+// const page = new CompanyListPage();
 
-describe('Company list', () => {
+// describe('Company list', () => {
 
-  before(() => {
-    cy.task('seed:database');
-  });
+//   before(() => {
+//     cy.task('seed:database');
+//   });
 
-  beforeEach(() => {
-    page.navigateTo();
-  });
+//   beforeEach(() => {
+//     page.navigateTo();
+//   });
 
-  it('Should have the correct title', () => {
-    page.getUserTitle().should('have.text', 'Companies');
-  });
+//   it('Should have the correct title', () => {
+//     page.getUserTitle().should('have.text', 'Companies');
+//   });
 
-  // There are 10 users in the database, two of which work
-  // for OHMNET, so there are 9 different companies.
-  it('Should show 9 companies in the list', () => {
-    page.getCompanyCards().should('have.length', 9);
-  });
+//   // There are 10 users in the database, two of which work
+//   // for OHMNET, so there are 9 different companies.
+//   it('Should show 9 companies in the list', () => {
+//     page.getCompanyCards().should('have.length', 9);
+//   });
 
-  it('Should contain a company with the name "OHMNET"', () => {
-    page.getCompanyCardByName('OHMNET').should('exist');
-  });
+//   it('Should contain a company with the name "OHMNET"', () => {
+//     page.getCompanyCardByName('OHMNET').should('exist');
+//   });
 
-  it('Should contain a company with the name "OHMNET" and 2 users', () => {
-    page.getCompanyCardUserNames('OHMNET').should('have.length', 2);
-  });
+//   it('Should contain a company with the name "OHMNET" and 2 users', () => {
+//     page.getCompanyCardUserNames('OHMNET').should('have.length', 2);
+//   });
 
-  it('Should contain a company with the name "VINCH" and 1 user', () => {
-    page.getCompanyCardUserNames('VINCH').should('have.length', 1);
-  });
-});
+//   it('Should contain a company with the name "VINCH" and 1 user', () => {
+//     page.getCompanyCardUserNames('VINCH').should('have.length', 1);
+//   });
+// });
